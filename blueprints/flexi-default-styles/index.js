5b1d5d0c2f36ff83da8fc0078932582167f55e22
Robert Wagner <rwwagner90@gmail.com>
2017-02-02 10:34:20 -0500

    10

--- blueprints/flexi-default-styles/index.js
+++ blueprints/flexi-default-styles/index.js
@@ -1,8 +1,15 @@
 /*jshint node:true*/
 module.exports = {
-  normalizeEntityName: function() {},
+  normalizeEntityName: function() {
+  },
 
   afterInstall: function() {
-    return this.addAddonToProject({ name: '@html-next/flexi-config', target: '2.0.0-beta.6' });
+    return this.addAddonToProject({
+      name: '@html-next/flexi-config',
+      target: '2.0.0-beta.6',
+      blueprintOptions: {
+        save: true
+      }
+    });
   }
 };
